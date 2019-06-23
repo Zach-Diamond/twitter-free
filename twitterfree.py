014504e0c4a5144b97092d4608ad46ea2f55d1fa
Zach-Diamond <50925443+Zach-Diamond@users.noreply.github.com>
2019-06-23 17:12:54 -0400

    Adding additional analysis results

--- twitterfree.py
+++ twitterfree.py
@@ -15,6 +15,68 @@ load_dotenv()
 def add_commas(my_number):
     return "{:,}".format(my_number)
 
+#Annoying hour timerange classifier - had to make super long because couldn't figure out >=/<= issue in pandas dtype
+#https://stackoverflow.com/questions/30953299/pandas-if-row-in-column-a-contains-x-write-y-to-row-in-column-b
+def applyFunc(hourposted):
+    if hourposted == 0:
+        return 'Late Night'
+    elif hourposted == 1:
+        return 'Late Night'
+    elif hourposted == 2:
+        return 'Late Night'
+    elif hourposted == 3:
+        return 'Late Night'
+    elif hourposted == 4:
+        return 'Late Night'
+    
+    elif hourposted == 5:
+        return 'Early Morning'
+    elif hourposted == 6:
+        return 'Early Morning'
+    elif hourposted == 7:
+        return 'Early Morning'
+    elif hourposted == 8:
+        return 'Early Morning'
+    
+    elif hourposted == 9:
+        return 'Late Morning'
+    elif hourposted == 10:
+        return 'Late Morning'
+    elif hourposted == 11:
+        return 'Late Morning'
+    
+    elif hourposted == 12:
+        return 'Early Afternoon'
+    elif hourposted == 13:
+        return 'Early Afternoon'
+    elif hourposted == 14:
+        return 'Early Afternoon'
+    
+    elif hourposted == 15:
+        return 'Late Afternoon'
+    elif hourposted == 16:
+        return 'Late Afternoon'
+    elif hourposted == 17:
+        return 'Late Afternoon'
+    
+    elif hourposted == 18:
+        return 'Early Evening'
+    elif hourposted == 19:
+        return 'Early Evening'
+    elif hourposted == 20:
+        return 'Early Evening'
+
+    elif hourposted == 21:
+        return 'Late Evening'
+    elif hourposted == 22:
+        return 'Late Evening'
+    elif hourposted == 23:
+        return 'Late Evening'
+
+    else:
+        return 'No Timestamp'
+
+
 
 ########SCRIPT-WIDE VARIABLES
 
@@ -28,6 +90,10 @@ ACCESS_SECRET = os.environ.get("ACCESS_SECRET")
 todaydate = datetime.now().strftime('%Y-%m-%d')
 yesterday = date.today() - timedelta(days=1)
 yesterdaydate = yesterday.strftime('%Y-%m-%d')
+lastweek = date.today() - timedelta(days=7)
+lastweekdate = lastweek.strftime('%Y-%m-%d')
+
+
 
 ###RESULT LISTS
 results=[]
@@ -111,7 +177,6 @@ else:
 #CREATING DATAFRAME
 tdf = pd.DataFrame(results)
 print("Number of tweets extracted: {}.\n".format(len(tdf)))
-print('-----------------------------------')
 
 ###ADDING COLUMNS FROM TWITTER DATA
 if source == 'user':
@@ -142,20 +207,63 @@ tdf['tweet_date'] = tdf['tweeted_at'].dt.date
 tdf['tweet_day'] = tdf['tweeted_at'].dt.weekday_name
 tdf['tweet_hour'] = tdf['tweeted_at'].dt.hour
 tdf['tweet_length'] = [len(tweets) for tweets in tdf['tweet']]
+tdf['tweet_timerange'] = tdf['tweet_hour']
+tdf['tweet_timerange'] = tdf['tweet_timerange'].apply(applyFunc)
+
 
 #calculations
 avg_tweet_length = np.average(tdf['tweet_length'])
 most_tweets_on = np.max(tdf['tweet_date'])
 number_max_tweets = len(tdf.loc[(tdf['tweet_date']==np.max(tdf['tweet_date'])),:])
-most_frequent_day = np.max(tdf['tweet_day'])
+percent_capslock = "{:.1f}".format(sum(map(str.isupper,str([i for i in tdf['tweet']]).split()))/len(str([i for i in tdf['tweet']]).split())*100)
+fully_capslock_tweet = sum([i.isupper() for i in tdf['tweet']])
+
+#Date Diff
+first_tweet = min(tdf['tweet_date'])
+last_tweet = max(tdf['tweet_date'])
+delta = (last_tweet - first_tweet).days
+
+#Count Tables/Data
+retweet_most_tweet = tdf.sort_values('retweet_count', ascending=False)[:1]['tweet'].values[0]
+retweet_most_date = tdf.sort_values('retweet_count', ascending=False)[:1]['tweet_date'].values[0]
+retweet_most_retweets = add_commas(tdf.sort_values('retweet_count', ascending=False)[:1]['retweet_count'].values[0])
+likes_most_tweet = tdf.sort_values('likes', ascending=False)[:1]['tweet'].values[0]
+likes_most_date = tdf.sort_values('likes', ascending=False)[:1]['tweet_date'].values[0]
+likes_most_retweets = add_commas(tdf.sort_values('likes', ascending=False)[:1]['likes'].values[0])
+
+
+table_most_frequent_day = tdf.groupby(['tweet_day'], as_index=False).agg({
+    "id":np.count_nonzero,
+    "likes":np.sum,
+    "retweet_count":np.sum
+    }).sort_values('id',ascending=False).rename({'id':'count'},axis='columns')
+    
+most_frequent_day = table_most_frequent_day[:1]['tweet_day'].values[0]
 
 #append a hashtag if source = hashtag
 if source == 'hashtag':
     selected_name = '#'+selected_name
 
+### {# of tweets extracted -> from X to Y [Z Days]}
+print(f'Data from {first_tweet} to {last_tweet} ({delta} days).')
+print()
+print('-----------------------------------')
+print('-----------------------------------')
+
 ####RESULTS
-print(f'Average Tweet length: {avg_tweet_length}')
-print(f'Most Tweets on: {most_tweets_on} for a total of {number_max_tweets}')
-print(f"{most_frequent_day} is {selected_name}'s most frequent day for Tweets.")
+print(f'Average Tweet length: {"{:.1f}".format(avg_tweet_length)} characters.')
+print(f'Most Tweets on: {most_tweets_on} for a total of {number_max_tweets} tweets.')
+print(f'Most retweeted: "{retweet_most_tweet}" with {retweet_most_retweets} retweets (posted on {retweet_most_date}).')
+print(f'Most liked: "{likes_most_tweet}" with {likes_most_retweets} likes (posted on {likes_most_date}).')
+
+print('-----------------------------------')
+print(f"{fully_capslock_tweet} of {selected_name} {len(tdf)} Tweets were written entirely in ALL CAPS!")
+print(f"{percent_capslock}% of {selected_name}'s individual words were ALL CAPS!")
+
+print('-----------------------------------')
 print("Primary source of tweets:")
-print(tdf.groupby(['source'],as_index=False)[['id']].count().sort_values('id',ascending=False))
+print(tdf.groupby(['source'],as_index=False)[['id']].count().sort_values('id',ascending=False).rename({'id':'count'},axis='columns').to_string(index=False)) ## Remove to_string for DF
+
+print('-----------------------------------')
+print(f"{most_frequent_day} is {selected_name}'s most frequent day for Tweets.")
+print(table_most_frequent_day.to_string(index=False)) ## Remove to_string for DF
